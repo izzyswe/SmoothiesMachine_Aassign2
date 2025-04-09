bdbdab78dd893e3f9e34df9fe7d0ae753672170b
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-04-08 22:26:20 -0400

    removed unneccessary fields and fixed constructor

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// create all the elements needed for the smoothie
-const div = document.getElementById("smoothie-list");
-
 // Smoothie class
 class SmoothieIngredient {
   /*creating a nested key-value pair for the fruit prices
@@ -9,17 +6,36 @@ class SmoothieIngredient {
    the fruit prices and calories in one object.
    I felt lazy to create a separate object for the calories */
   fruitPrices= {
-    "Banana": {price: 1, calories: 73}, 
-    "Apple": {price: 1, calories: 95},
-    "Orange": {price: 1, calories: 62},
-    "Strawberry": {price: 1, calories: 33},
-    "Mango": {price: 1, calories: 60},
-    "Pineapple": {price: 1, calories: 82},
-    "Blueberry": {price: 1, calories: 57},
-    "Peach": {price: 1, calories: 58},
-    "Kiwi": {price: 1, calories: 42},
-    "Grapes": {price: 1, calories: 69},
-    "Watermelon": {price: 1, calories: 30},
+    "Banana": { 
+      price: 1, 
+      calories: 73
+    }, 
+    "Matcha": {
+      price: 1, 
+      calories: 95
+    },
+    "Strawberry": {
+      price: 1, 
+      calories: 33
+    },
+    "Mango": {
+      price: 1, 
+      calories: 60
+    },
+    "Pineapple": {
+      price: 1, 
+      calories: 82
+    },
+    "Blueberry": {
+      price: 1, 
+      calories: 57
+    },
+    "WheyProtein": {
+      price: 1.50, 
+      calories: 30
+    },
+    // as obnoxious as it is to position the objects like this instead of the previous commit
+    // I did it to make it easier to read, even though the other way was just as easy
   };
   //creating a key-value pair for the smoothie size
   size = {
@@ -27,11 +43,13 @@ class SmoothieIngredient {
     "Medium": 2.67, 
     "Large": 3.56
   };
+  //for readability sake, it's redundant but its confusing translating java to js
+  selectedSize;
+  selectedFruits;
   // created a constructor to create the smoothie object
-  constructor(fruitPrices, size, calories) {
-    this.fruitPrices = fruitPrices;
-    this.size = size;
-    this.calories = calories;
+  constructor(selectedFruits, selectedSize) {
+    this.selectedFruits = selectedFruits;
+    this.selectedSize = selectedSize;
   }
 
   //creating a method to get the smoothie price
@@ -54,7 +72,8 @@ class SmoothieIngredient {
             Fruits ${this.getSmoothieIngredients} \n
             Calories: ${this.getSmoothieCalories} \n
             ______________________________\n
-            Total: $${getSmoothiePrice}`;
+            Total: \n
+            $${this.getSmoothiePrice}`;
   }
 }
 
